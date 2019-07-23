5581dcdd07cd3d4b2ee0445f4afb98d0b7a16b8e
“Dean” <467271883@qq.com>
2019-07-23 11:50:58 +0800

    Add property to main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import '../static/css/global.css'
+import '../static/css/checkbox.css'
+import {post,get,patch,put} from './http/http'
 
 Vue.config.productionTip = false
+Vue.prototype.$post=post;
+Vue.prototype.$get=get;
+Vue.prototype.$patch=patch;
+Vue.prototype.$put=put;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Container from '@/components/Container'
 
 Vue.use(Router)
